82db70a3c453e2ae4ada8c612b774e2beabe5d82
ErtugBuldak <ertugbuldak1907@gmail.com>
2026-02-16 15:57:38 +0300

    Solve exercise 6k

---
+++ ecommerce-project/src/pages/ErrorPage.jsx
@@ -0,0 +1,18 @@
+import Header from '../components/Header'
+import './ErrorPage.css'
+
+function ErrorPage() {
+  return (
+    <>
+      <title>Page Not Found</title>
+
+      <Header />
+
+      <div>
+        Page Not Found
+      </div>
+    </>
+  )
+}
+
+export default ErrorPage
